d23e27941b57a53a1053b7b346c75416cea20e5c
Christopher Beltran <christod.beltran@gmail.com>
2018-08-04 05:14:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,11 +23,12 @@ def artist=(artist)
   artist.add_song(self)
 end 
 
-def self.new_from_filename(filename)
+def self.new_by_filename(filename)
   artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
   song = self.new(song_name)
   song.artist = Artist.find_or_create_by_name(artist_name)
   song.genre = genre_name
+  song.save
 
 end 
 

e4d46ad6714cde58dfd01956afb506aa048211af
Christopher Beltran <christod.beltran@gmail.com>
2018-08-04 06:04:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,8 @@ def save
 end 
 
 def self.find_or_create_by_name(name)
-    
+    self.all.detect {|artist| artist.name == name } || Artist.new(name)
+  end 
   
 def print_songs
   @songs.each {|s| puts s.name}

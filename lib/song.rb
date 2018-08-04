0a1fe6da9e29b83e1eb8fde4554ff15d432e64d4
Christopher Beltran <christod.beltran@gmail.com>
2018-08-04 05:45:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,3 +6,13 @@ def initialize(name)
   @name = name
   @artist = nil
 end 
+
+def self.new_by_filename(filename)
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
+  end 
+end 

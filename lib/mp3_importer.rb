b562062ad773fa30faafb271bb5f445b9c9cc953
Christopher Beltran <christod.beltran@gmail.com>
2018-08-04 05:51:26 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,8 +11,8 @@ class MP3Importer
   end 
   
   def import
-    self.files.each do 
-      {|f| Song.new_by_filename(f)}
+    self.files.each do |f| 
+      Song.new_by_filename(f)}
   end 
   
 end 

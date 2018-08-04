19058292af5a101f4745701c0cd1e8eba9a032d1
Christopher Beltran <christod.beltran@gmail.com>
2018-08-04 05:42:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,30 +6,3 @@ def initialize(name)
   @name = name
   @artist = nil
 end 
-
-@@all = []
-
-def self.all
-  @@all
-end 
-
-def save
-  @@all << self
-  self
-end 
-
-def artist=(artist)
-  @artist = artist
-  artist.add_song(self)
-end 
-
-def self.new_by_filename(filename)
-  artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
-  song = self.new(song_name)
-  song.artist = Artist.find_or_create_by_name(artist_name)
-  song.genre = genre_name
-  song.save
-
-end 
-
-end 
